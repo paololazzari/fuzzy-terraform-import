6d142851d5ca5126b933ebb54ad570ed6b7042da
Paolo Lazzari <lazzari.paolok@gmail.com>
2023-01-15 16:23:28 +0000

    Remove scripts used locally for testing

--- test/runtests.sh
+++
@@ -1,4 +0,0 @@
-sudo docker rm -f testcontainer >/dev/null
-sudo docker run --name testcontainer -d -p 5000:5000 -v /mnt/c/Users/Paolo/Documents/GitHub/fuzzy-terraform-import/test:/src/ testimage
-sudo docker exec testcontainer /test/test_wrapper.sh
-sudo docker exec testcontainer go test -v ./...
